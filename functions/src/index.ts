978a7972fd29ac9aa43875ead8ccce660e5fa345
flatfisher <shimano.entou@gmail.com>
2019-01-15 23:01:08 +0900

    Add function to get a photo as buffer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,21 +22,32 @@ function doRequest(options: any) {
     })
 }
 
+async function getFile(response: any): Promise<File> {
+    const channel: Channel = <Channel>JSON.parse(response)
+    const file: File = channel.files[0]
+    return file
+}
+
 exports.saveSlackPhotos = functions.pubsub.topic('slack-to-googlephotos').onPublish(async (_) => {
     // Environment Variables
     const SLACK_TOKEN = `${functions.config().slack.token}`
     const SLACK_CHANNEL = `${functions.config().slack.channel}`
 
     //README: Get slack files
-    const getSlackPhotos = {
+    const getSlackOption = {
         url: `https://slack.com/api/files.list?token=${SLACK_TOKEN}&channel=${SLACK_CHANNEL}&count=5&pretty=1`,
         method: "GET",
     }
-    const slackResponse = await doRequest(getSlackPhotos).catch(err => console.error(err))
-    const channel: Channel = <Channel>JSON.parse(<any>slackResponse)
-    const file: File = channel.files[0]
-    console.log(slackResponse)
-    console.log(file.id)
-    console.log(file.url_private)
-    console.log(file.filetype)
+    const slackResponse = await doRequest(getSlackOption).catch(err => console.error(err))
+    const file: File =  <File> await getFile(slackResponse).catch(err => console.error(err))
+
+    //README: Get a photo
+    const getPhotoOption = {
+        url: `${file.url_private}`,
+        headers: {
+            'Authorization': `Bearer ${SLACK_TOKEN}`
+        },
+        encoding: null
+    }
+    const photo = await doRequest(getPhotoOption).catch(err => console.error(err)) //Buffer
 })
